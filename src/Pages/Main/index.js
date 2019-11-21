c755f812b7d0e68dccd222646198aa0e708e5e51
Vinicius Bium Donadon <vbiumdon@everis.com>
2019-11-21 10:41:56 -0300

    Adjusts

---
+++ src/Pages/Main/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Title } from './styles';
+
+export default function Main() {
+  return (
+    <div>
+      <Title>This boilerplate comes with:</Title>
+      <ul>
+        <li>eslint</li>
+        <li>prettier</li>
+        <li>editorconfig</li>
+        <li>styled components</li>
+        <li>router</li>
+      </ul>
+    </div>
+  );
+}
